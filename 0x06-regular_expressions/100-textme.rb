a8bfb83b7a13cf96e17b5ac7018e9c04a9b5aca3
Drihmia <drihmia.redouane@gmail.com>
2024-01-03 19:14:04 +0100

    fixed a typo

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 matching_group = ARGV[0].match(/.*\[from:(?<from>\w+|\+?\d+)\].*\[to:(?<to>\w+|\+?\d+)\].*\[flags:(?<flags>.*?)\].*/)
-puts "#{matching_group[:from]}, #{matching_group[:to]}, #{matching_group[:flags]}"
+puts "#{matching_group[:from]},#{matching_group[:to]},#{matching_group[:flags]}"
