e6c0269580fc7cf1fd19e7da7ff639b7859940bd
Drihmia <drihmia.redouane@gmail.com>
2024-02-23 23:51:23 +0100

    used single quote on the pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 # Using Puppet install flask from pip3.
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3,
-}
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => pip3,
-  require  => package['flask']
+  provider => 'pip3',
 }
