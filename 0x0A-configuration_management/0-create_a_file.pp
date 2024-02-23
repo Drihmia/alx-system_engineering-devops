03a3522aa2ac2bc1496130890aba88f07e603410
Drihmia <drihmia.redouane@gmail.com>
2024-02-23 22:55:36 +0100

    align verticaly the closing curly brace at column 0 and remove empty line after documentTION

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # create a file in /tmp.
-
 file {'/tmp/school':
   ensure  => file,
-  content => 'I love Puppet',
-  owner   => 'redouane',
-  group   => wheel,
   mode    => '0744',
+  owner   => www-data,
+  group   => www-data,
+  content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,4 +2,4 @@
 exec { 'kill ./killmenow':
   command => '/usr/bin/pkill -xf \'/bin/bash ./killmenow\'',
   onlyif  => '/usr/bin/pgrep -af \'/bin/bash ./killmenow\'',
-  }
+}
