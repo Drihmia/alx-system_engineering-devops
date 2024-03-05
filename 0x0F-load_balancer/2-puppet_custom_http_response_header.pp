9d67df329207c4e11195d68720446fdeefce501d
Drihmia <drihmia.redouane@gmail.com>
2024-03-05 03:44:45 +0100

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,28 @@
+# Update system packages
+package { 'update':
+  ensure => latest,
+}
+
+# Install Nginx package
+package { 'nginx':
+  ensure  => installed,
+  require => Package['update'],
+}
+
+# Add custom HTTP header to Nginx configuration
+file_line { 'add_custom_header':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => '^\s*server_name _;',
+  match   => '^(\s*)server_name _;$',
+  after   => 'server_name _;',
+  content => "        \\1add_header X-Served-By \"${trusted['hostname']}\";",
+  require => Package['nginx'],
+}
+
+# Restart Nginx service
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File_line['add_custom_header'],
+}
