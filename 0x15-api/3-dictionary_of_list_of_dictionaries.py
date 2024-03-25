142b9890af314c543a2d662719e1d799fe3ab1e4
Drihmia <drihmia.redouane@gmail.com>
2024-03-25 22:39:25 +0000

    removed an forgotted printing statement i ve used for testing and correct a pycode style

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -31,13 +31,13 @@ if __name__ == "__main__":
         url_full_todo = f"{url}/todos?userId={employee_id}"
         with get(url_full_todo) as response:
             list_tasks = response.json()
-            print(employee_id, len(list_tasks))
 
         for task in list_tasks:
             t_st = task.get("completed", "not found")
             title = task.get("title", "not found")
-            json_dict_l.append({"username": empl_un, "task": title,
-                           "completed": t_st})
+            json_dict_l.append({"username": empl_un,
+                                "task": title,
+                                "completed": t_st})
 
     if json_dict:
         with open("todo_all_employees.json", "w", encoding="utf-8") as f:
