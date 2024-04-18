5314a95f9d21f362384cc3e1361cb3e23f529d68
Drihmia <drihmia.redouane@gmail.com>
2024-04-18 20:08:21 +0100

    puppet lint on 1-user_limnit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,12 +3,12 @@ file { '/etc/default/nginx':
 }
 
 exec { 'replace_hard':
-  command     => "sed -i '/^holberton[[:space:]]*hard/s/.*/holberton hard nofile 10000/' /etc/security/limits.conf",
-  path        => ['/usr/bin', '/usr/sbin', '/bin'],
-  subscribe   => File['/etc/default/nginx'],
+  command   => "sed -i '/^holberton[[:space:]]*hard/s/.*/holberton hard nofile 10000/' /etc/security/limits.conf",
+  path      => ['/usr/bin', '/usr/sbin', '/bin'],
+  subscribe => File['/etc/default/nginx'],
 }
 exec { 'replace_soft':
-  command     => "sed -i '/^holberton[[:space:]]*soft/s/.*/holberton soft nofile 10000/' /etc/security/limits.conf",
-  path        => ['/usr/bin', '/usr/sbin', '/bin'],
-  subscribe   => File['/etc/default/nginx'],
+  command   => "sed -i '/^holberton[[:space:]]*soft/s/.*/holberton soft nofile 10000/' /etc/security/limits.conf",
+  path      => ['/usr/bin', '/usr/sbin', '/bin'],
+  subscribe => File['/etc/default/nginx'],
 }
