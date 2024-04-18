9d87ec27ffd9b4f8d15697c580ecbf42862bf805
Drihmia <drihmia.redouane@gmail.com>
2024-04-18 20:34:36 +0100

    make sure nginx is installed and enabled and restarted once the file is modified

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,4 +2,16 @@
 file {'/etc/default/nginx':
   ensure  => present,
   content => 'ULIMIT="-n 6000"',
+  notify  => Service['nginx'],
+  }
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  }
+
+package { 'nginx':
+  ensure => installed,
   }
